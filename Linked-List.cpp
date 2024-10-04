44f630fec3ce40842d3e90aa8cc88a04fb5f03cc
likely-shayan <shayan.official.3406@gmail.com>
2024-10-04 14:04:00 +0530

    Modified Linked List

--- Linked-List.cpp
+++ Linked-List.cpp
@@ -1,56 +1,252 @@
 #include <iostream>
 
-struct Node {
+struct SinglyNode {
 	int data;
-	Node* next = nullptr;
-	Node* prev = nullptr;
+	SinglyNode* next;
+	
+	SinglyNode(int val) : data{val} {
+		next = nullptr;
+	}
 };
 
-Node* Search(Node* head, int value) {
-	Node* curr = head;
-	while (curr != nullptr && curr->data != value) {
-		curr = curr->next;
+struct DoublyNode {
+	int data;
+	DoublyNode* prev;
+	DoublyNode* next;
+
+	DoublyNode(int val) : data{val} {
+		prev = nullptr;
+		next = nullptr;
 	}
-	return curr;
-}
+};
 
-void Insert(Node* node, Node* newNode) {
-	newNode->prev = node->prev;
-	newNode->next = node;
-	if (node != nullptr) {
-		node->prev = newNode;
+class SinglyLinkedList {
+public:
+	SinglyNode* head = nullptr;
+
+	SinglyLinkedList() = default;
+
+	SinglyLinkedList(int* Elements, int size) {
+		SinglyNode* curr = nullptr;
+		SinglyNode* prev = nullptr;
+		for (int i = 0; i < size; ++i) {
+			curr = new SinglyNode(Elements[i]);
+			if (head == nullptr) {
+				head = curr;
+			}
+			else {
+				prev->next = curr;
+			}
+			prev = curr;
+		}
 	}
-}
 
-void Delete(Node* nodeToDelete) {
-	nodeToDelete->next->prev = nodeToDelete->prev;
-	nodeToDelete->prev->next = nodeToDelete->next;
-}
+	SinglyLinkedList(int* begin, int* end) {
+		SinglyNode* curr = nullptr;
+		SinglyNode* prev = nullptr;
+		while (begin != end) {
+			curr = new SinglyNode(*begin);
+			if (head == nullptr) {
+				head = curr;
+			}
+			else {
+				prev->next = curr;
+			}
+			prev = curr;
+			++begin;
+		}
+	}
 
-int main() {
-	Node *n1 = new Node;
-	Node *n2 = new Node;
-	Node *n3 = new Node;
-	Node *n4 = new Node;
-	n1->data = 1;
-	n2->data = 2;
-	n3->data = 3;
-	n4->data = 4;
+	// Return the first node from head storing value as data
+	SinglyNode* Search(int value) {
+		SinglyNode* curr = head;
+		while (curr != nullptr && curr->data != value) {
+			curr = curr->next;
+		}
+		return curr;
+	}
 
-	Insert(n4, n3);
-	Insert(n3, n2);
-	Insert(n2, n1);
-	
-	std::cout << n1->data << "->" << n1->next->data << "->" << n4->prev->data << "->" << n4->data << "->" << "NULL" << std::endl;
+	// Insert newNode after the prev node
+	void Insert(SinglyNode* prev, SinglyNode* newNode) {
+		SinglyNode* temp = prev->next;
+		prev->next = newNode;
+		newNode->next = temp;
+	}
 
-	Delete(n3);
+	// Insert a new Node storing value newVal after the prev node
+	void Insert(SinglyNode* prev, int newVal) {
+		SinglyNode* newNode = new SinglyNode(newVal);
+		Insert(prev, newNode);
+	}
 
-	std::cout << n1->data << "->" << n1->next->data << "->" << n4->prev->data << "->" << n4->data << "->" << "NULL" << std::endl;
+	// Insert a new Node storing value newVal after the first node storing gvien value
+	void Insert(int value, int newVal) {
+		SinglyNode* prev = Search(value);
+		Insert(prev, newVal);
+	}
 
-	delete n1;
-	delete n2;
-	delete n3;
-	delete n4;
+	// Insert new node after the first node storing given value
+	void Insert(int value, SinglyNode* newNode) {
+		SinglyNode* prev = Search(value);
+		Insert(prev, newNode);
+	}
 
-	return 0;
-}
+	// Remove the given node
+	void Remove(SinglyNode* node) {
+		SinglyNode* temp = head;
+		if (node == head) {
+			head = head->next;
+			delete temp;
+			return;
+		}
+		while (temp->next != node) {
+			temp = temp->next;
+		}
+		temp->next = node->next;
+		delete node;
+	}
+
+	// Remove the first node storing given value
+	void Remove(int value) {
+		SinglyNode* node = Search(value);
+		Remove(node);
+	}
+
+	// Update the value of node to newVal
+	void Update(SinglyNode* node, int newVal) {
+		node->data = newVal;
+	}
+
+	// Update the value of first node storing value currVal to newVal
+	void Update(int currVal, int newVal) {
+		SinglyNode* node = Search(currVal);
+		Update(node, newVal);
+	}
+
+	~SinglyLinkedList() {
+		SinglyNode* curr = head;
+		while (curr != nullptr) {
+			SinglyNode* temp = curr;
+			curr = curr->next;
+			delete temp;
+		}
+	}
+
+};
+
+class DoublyLinkedList {
+public:
+	DoublyNode* head = nullptr;
+
+	DoublyLinkedList() = default;
+
+	DoublyLinkedList(int* Elements, int size) {
+		DoublyNode* curr = nullptr;
+		DoublyNode* prev = nullptr;
+		for (int i = 0; i < size; ++i) {
+			curr = new DoublyNode(Elements[i]);
+			if (head == nullptr) {
+				head = curr;
+			}
+			else {
+				prev->next = curr;
+				curr->prev = prev;
+			}
+			prev = curr;
+		}
+	}
+
+	DoublyLinkedList(int* begin, int* end) {
+		DoublyNode* curr = nullptr;
+		DoublyNode* prev = nullptr;
+		while (begin != end) {
+			curr = new DoublyNode(*begin);
+			if (head == nullptr) {
+				head = curr;
+			}
+			else {
+				prev->next = curr;
+				curr->prev = prev;
+			}
+			prev = curr;
+			++begin;
+		}	
+	}
+
+	DoublyNode* Search(int value) {
+		DoublyNode* curr = head;
+		while (curr != nullptr && curr->data != value) {
+			curr = curr->next;
+		}
+		return curr;
+	}
+
+	// Insert newNode after the given node
+	void Insert(DoublyNode* node, DoublyNode* newNode) {
+		DoublyNode* temp = node->next;
+		if (temp != nullptr) {
+			temp->prev = newNode;
+		}
+		node->next = newNode;
+		newNode->next = temp;
+		newNode->prev = node;
+	}
+
+	// Insert a new Node storing value newVal after the prev node
+	void Insert(DoublyNode* node, int newVal) {
+		DoublyNode* newNode = new DoublyNode(newVal);
+		Insert(node, newNode);
+	}
+
+	// Insert a new Node storing value newVal after the first node storing gvien value
+	void Insert(int value, int newVal) {
+		DoublyNode* node = Search(value);
+		Insert(node, newVal);
+	}
+
+	// Insert new node after the first node storing given value
+	void Insert(int value, DoublyNode* newNode) {
+		DoublyNode* node = Search(value);
+		Insert(node, newNode);
+	}
+
+	// Remove the given node
+	void Remove(DoublyNode* node) {
+		if (node == head) {
+			head = head->next;
+			delete node;
+			return;
+		}
+		if (node->next != nullptr) {
+			node->next->prev = node->prev;
+		}
+		node->prev->next = node->next;
+		delete node;
+	}
+
+	// Remove the first node storing given value
+	void Remove(int value) {
+		DoublyNode* node = Search(value);
+		Remove(node);
+	}
+
+	// Update the value of node to newVal
+	void Update(DoublyNode* node, int newVal) {
+		node->data = newVal;
+	}
+
+	// Update the value of first node storing value currVal to newVal
+	void Update(int currVal, int newVal) {
+		DoublyNode* node = Search(currVal);
+		Update(node, newVal);
+	}
+
+	~DoublyLinkedList() {
+		DoublyNode* curr = head;
+		while (curr != nullptr) {
+			DoublyNode* temp = curr;
+			curr = curr->next;
+			delete temp;
+		}
+	}
+};
